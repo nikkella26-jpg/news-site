0168a4afd9568f9d352c7c412fa2d527ca5d34cb
Manoj-Axelsson <mr.mjaxelsson@gmail.com>
2026-02-01 16:37:21 +0100

    change of index from ts to tsx

--- app/weather/page.tsx
+++ app/weather/page.tsx
@@ -1,15 +1,10 @@
 import { getWeatherByLocation } from "@/actions/weather";
 
 export default async function Page() {
-  try {
-    const weather = await getWeatherByLocation("Linköping");
-    return (
-      <div>
-        <pre>{JSON.stringify(weather, null, 2)}</pre>
-      </div>
-    );
-  } catch (error) {
-    console.error(error);
-    return <div>Failed to load weather data. Please try again later.</div>;
-  }
+  const weather = await getWeatherByLocation("Linköping");
+  return (
+    <div>
+      <pre>{JSON.stringify(weather, null, 2)}</pre>
+    </div>
+  );
 }
